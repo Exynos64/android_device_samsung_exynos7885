4a0299d70223d45402e15816b273fe570150dc63
Maxim Leo <mxsleo@outlook.com>
2024-06-26 18:15:10 +0300

    gta3xlwifi: full_gta3xlwifi.mk: disable telephony since we are building for a wifi only tablet

--- gta3xlwifi/full_gta3xlwifi.mk
+++ gta3xlwifi/full_gta3xlwifi.mk
@@ -5,7 +5,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/gta3xlwifi/device.mk)
